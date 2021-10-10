b2b68a67aa7eeff35eef689814359466b12caee3
MUsoftware <musoftware@daum.net>
2021-10-10 19:41:16 +0900

    Add splash screen

--- app/src/main/java/cc/mudev/bca_android/activity/core/CoreSplashActivity.java
+++ app/src/main/java/cc/mudev/bca_android/activity/core/CoreSplashActivity.java
@@ -1,11 +1,18 @@
 package cc.mudev.bca_android.activity.core;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import cc.mudev.bca_android.R;
+import cc.mudev.bca_android.network.NetworkSupport;
+import cc.mudev.bca_android.service.FCMHandlerService;
 
 public class CoreSplashActivity extends AppCompatActivity {
     private long backKeyPressedTime = 0;
@@ -13,7 +20,20 @@ public class CoreSplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_core_splash);
+
+        this.createNotificationChannel();
+        NetworkSupport.getInstance().initialize(getApplicationContext());
+
+        Intent intent = new Intent(this, CoreFrontActivity.class);
+        startActivity(intent);
+
+
+        String fcmToken = FCMHandlerService.getToken(getApplicationContext());
+        Log.w("FCMCoreSplashActivity", fcmToken);
+
+//        FCMHandlerService fcmHandlerService = new FCMHandlerService();
+//        fcmHandlerService.sendNotification("isItWorking?");
+        finish();
     }
 
     @Override
@@ -27,6 +47,24 @@ public class CoreSplashActivity extends AppCompatActivity {
         }
         if (System.currentTimeMillis() <= backKeyPressedTime + 2500) {
             finish();
+        }
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = getString(R.string.default_notification_channel_id);
+            String name = getString(R.string.default_notification_channel_name);
+            String description = getString(R.string.default_notification_channel_description);
+
+            NotificationChannel channel = new NotificationChannel(channelId, name, NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription(description);
+
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
     }
 }
